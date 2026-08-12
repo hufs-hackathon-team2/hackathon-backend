9597165bd548b21ffeeacf02873dabe3a0c3bef0
nutdaeun <goodnvery@naver.com>
2026-08-12 21:21:47 +0900

    feat: 퀘스트 시작 API 구현

---
+++ quest/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Quest
+
+#POST 요청 body용
+class QuestCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Quest
+        fields = ['quest_content']
+
+#POST 요청의 응답 body용
+class QuestResponseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Quest
+        fields = ['id', 'quest_content', 'started_at']
--- quest/views.py
+++ quest/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.generics import status, Response
+from .serializers import QuestCreateSerializer, QuestResponseSerializer
+from .models import Quest
+from datetime import date
+from cycle.services import close_and_start_new_cycle
 
-# Create your views here.
+class StartQuestAPIView(APIView):
+
+    def post(self, request):
+        create_serializer = QuestCreateSerializer(data=request.data)
+        create_serializer.is_valid(raise_exception=True)  # 실패 시 자동으로 400 응답
+
+        quest = Quest.objects.create(
+            quest_content = create_serializer.validated_data['quest_content'],
+            state='ACTIVE',
+            started_at=date.today(),
+            cycle=request.user.current_cycle,
+        )
+
+        close_and_start_new_cycle(request.user, date.today(), linked_record=quest)
+
+        response_serializer = QuestResponseSerializer(quest)
+        return Response(response_serializer.data, status=status.HTTP_201_CREATED)
