d220ca4dae673ba9137262338ec7af711cce3545
nutdaeun <goodnvery@naver.com>
2026-08-13 20:58:05 +0900

    chore: url 뷰 오타 수정

--- cycle/urls.py
+++ cycle/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cycle/analysis/<int:cycle_count>', views.FormerCycleAnalysisSerializer.as_view(), name='former_cycle_analysis'),
+    path('cycle/analysis/<int:cycle_count>', views.GetCycleAnalysis.as_view(), name='former_cycle_analysis'),
 ]
